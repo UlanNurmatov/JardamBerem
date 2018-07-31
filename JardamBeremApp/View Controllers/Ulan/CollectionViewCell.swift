25af26108e25bb2f9d8b60ff231d78f8947a5c0b
Ulan Nurmatov <ulannurmatov@ulans-macbook-pro.local>
2018-07-31 21:05:13 +0600

    Created ViewControllers

---
+++ JardamBeremApp/View Controllers/Ulan/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  JardamBeremApp
+//
+//  Created by Ulan Nurmatov on 7/31/18.
+//  Copyright © 2018 Ulan Nurmatov. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
