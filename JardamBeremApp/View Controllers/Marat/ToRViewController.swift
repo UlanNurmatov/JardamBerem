f3258b6b479324e953e82a2f962eb87ef3c020a3
SomeGoodMan <marat31bv@gmail.com>
2018-07-31 20:46:53 +0600

    Added isFirsTimeLoaded to ToR VC

--- JardamBeremApp/View Controllers/Marat/ToRViewController.swift
+++ JardamBeremApp/View Controllers/Marat/ToRViewController.swift
@@ -13,23 +13,14 @@ class ToRViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let firstTimeLoad : Bool = UserDefaults.standard.object(forKey: "isAgreesWithTOR") == nil
+        
+        if firstTimeLoad {
+
+        } else {
+            let board = UIStoryboard(name: "MaratStoryboard", bundle: nil)
+            let vc = board.instantiateViewController(withIdentifier: "LanguageChooseVC")
+            self.show(vc, sender: self)
+        }        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
