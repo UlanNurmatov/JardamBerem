df340ffeb81b77ae1257efaa1467bebfaa53b1eb
SomeGoodMan <marat31bv@gmail.com>
2018-08-02 21:18:16 +0600

    Made part of Charity Detail VC

--- JardamBeremApp/View Controllers/Marat/DetailCharityViewController.swift
+++ JardamBeremApp/View Controllers/Marat/DetailCharityViewController.swift
@@ -7,29 +7,25 @@
 //
 
 import UIKit
+import Kingfisher
 
 class DetailCharityViewController: UIViewController {
 
+    @IBOutlet weak var imgView : UIImageView!
+    @IBOutlet weak var titleLabel : UILabel!
+    @IBOutlet weak var descriptionLabel : UILabel!
+    
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setData (imageURL : String,title : String, description : String) {
+        
+        let url = URL(string : imageURL)
+        self.imgView.kf.setImage(with: url)
+        
     }
-    */
-
 }
