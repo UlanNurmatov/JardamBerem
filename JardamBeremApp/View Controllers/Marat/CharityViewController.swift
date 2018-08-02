4247ee9ddaed5a4259ac9af84d4724f6b2d3996b
SomeGoodMan <marat31bv@gmail.com>
2018-08-02 19:39:10 +0600

    Added Charity controller and Charity TableViewCell

--- JardamBeremApp/View Controllers/Marat/CharityViewController.swift
+++ JardamBeremApp/View Controllers/Marat/CharityViewController.swift
@@ -8,28 +8,25 @@
 
 import UIKit
 
-class CharityViewController: UIViewController {
+class CharityViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataManager.manager.charities!.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
     }
-    */
 
 }
