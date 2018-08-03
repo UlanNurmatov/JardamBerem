34b58909d4422fe9da2b0617b35f49ecf2a8b2e3
Ulan Nurmatov <ulannurmatov@Ulans-MacBook-Pro.local>
2018-08-04 02:14:48 +0600

    Announcement details ViewController created

---
+++ JardamBeremApp/View Controllers/Ulan/AnnouncementDetailsView.swift
@@ -0,0 +1,34 @@
+//
+//  AnnouncementDetailsView.swift
+//  JardamBeremApp
+//
+//  Created by Ulan Nurmatov on 8/4/18.
+//  Copyright © 2018 Ulan Nurmatov. All rights reserved.
+//
+
+import UIKit
+
+class AnnouncementDetailsView: UIViewController {
+    
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
+    @IBOutlet weak var image3: UIImageView!
+    @IBOutlet weak var titleOf: UILabel!
+    @IBOutlet weak var category: UILabel!
+    @IBOutlet weak var details: UILabel!
+    @IBOutlet weak var telephone: UILabel!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+   
+
+}
