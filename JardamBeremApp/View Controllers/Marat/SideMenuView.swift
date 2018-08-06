40841016cae3a59433d664ab58211744bdcbf955
SomeGoodMan <marat31bv@gmail.com>
2018-08-06 20:09:31 +0600

    Added side menu images, deleted SideMenu pod, created class(UIView) for side menu

---
+++ JardamBeremApp/View Controllers/Marat/SideMenuView.swift
@@ -0,0 +1,38 @@
+//
+//  SideMenuView.swift
+//  JardamBeremApp
+//
+//  Created by Marat on 06.08.2018.
+//  Copyright © 2018 Ulan Nurmatov. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuView: UIView {
+
+    @IBAction func mainMenuButtonTapped () {
+        
+    }
+    @IBAction func charitiesButtonTapped() {
+    
+    }
+    @IBAction func infoButtonTapped() {
+        
+    }
+    @IBAction func forumButtonTapped() {
+        
+    }
+    @IBAction func languageButtonTapped() {
+        
+    }
+    @IBAction func regionButtonTapped() {
+        
+    }
+    @IBAction func feedbackButtonTapped() {
+        
+    }
+    @IBAction func aboutUsButtonTapped() {
+        
+    }
+}
+
